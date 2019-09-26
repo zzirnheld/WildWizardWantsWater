632437fd2f3e46a4f4a6f0e67735b094ba189a2c
Bower <bowert@purdue.edu>
2019-09-26 15:38:18 -0400

    small scripting start

---
+++ Assets/RightHandPalette.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class RightHandPalette : MonoBehaviour
+{
+    #region steamvractions
+    public SteamVR_Action_Boolean PaletteAction;
+    #endregion steamvractions
+    // Update is called once per frame
+    void Update()
+    {
+        if (PaletteAction.state)
+        {
+            
+        }
+    }
+}

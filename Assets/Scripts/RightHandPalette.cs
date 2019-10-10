e9c22abd1be56a41c179e27973081d159912729d
Bower <bowert@purdue.edu>
2019-10-10 14:42:27 -0400

    nice button press

--- Assets/Scripts/RightHandPalette.cs
+++ Assets/Scripts/RightHandPalette.cs
@@ -11,32 +11,40 @@ public class RightHandPalette : MonoBehaviour
     public SteamVR_Action_Boolean paletteaction;
     #endregion steamvractions
     bool isSummoned = false;
+
+    void Start()
+    {
+        paletteaction.onStateDown += OnPaletteDown;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (paletteaction.state == true)
-        {
-            print("in");
-            if (!isSummoned)
-            {
-                Palette.transform.localScale = new Vector3(0F, 0F, 0F);
-                //double i = 0;
-                //while(i < 8){
-                    //Palette.transform.localScale += new Vector3(1F, 0.125F, 1F);
-                    //i = i + 1;
-                //}
-                Vector3 wandDir = transform.TransformDirection(Vector3.forward);
-                Palette.transform.position = Right_Hand.transform.position+(Right_Hand.transform.forward*2);//transform.TransformPoint(wandDir);//Right_Hand.transform.position + wandDir;
-                //Palette.transform.rotation =  Quaternion.LookRotation(wandDir);
-                Palette.transform.LookAt(Right_Hand.transform);
-                Palette.transform.rotation *= Quaternion.Euler(90, 0, 0);
-                isSummoned = true;
-            }
-            else
-            {
 
-                isSummoned = false;
-            }
+    }
+
+    public void OnPaletteDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        if (!isSummoned)
+        {
+            Palette.SetActive(true);
+            print("innert");
+            //Palette.transform.localScale = new Vector3(0F, 0F, 0F);
+            //double i = 0;
+            //while(i < 8){
+            //Palette.transform.localScale += new Vector3(1F, 0.125F, 1F);
+            //i = i + 1;
+            //}
+            Vector3 wandDir = transform.TransformDirection(Vector3.forward);
+            Palette.transform.position = Right_Hand.transform.position + (Right_Hand.transform.forward * 2);//transform.TransformPoint(wandDir);//Right_Hand.transform.position + wandDir;
+                                                                                                            //Palette.transform.rotation =  Quaternion.LookRotation(wandDir);
+            Palette.transform.LookAt(Right_Hand.transform);
+            Palette.transform.rotation *= Quaternion.Euler(90, 0, 0);
+            isSummoned = true;
+        }
+        else
+        {
+            Palette.SetActive(false);
+            isSummoned = false;
         }
     }
 }

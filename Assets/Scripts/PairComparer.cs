1e29daa68cd44972a93e7b0d599b1aedb9ce5871
Zirnheld <zzirnhel@purdue.edu>
2019-10-17 14:24:48 -0400

    paircomparer

---
+++ Assets/Scripts/PairComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PairComparer : IEqualityComparer<Pair>
+{
+    public bool Equals(Pair x, Pair y)
+    {
+        return (x.x == y.x && x.y == y.y) || (x.x == y.y && x.y == y.x);
+    }
+
+    public int GetHashCode(Pair obj)
+    {
+        return obj.GetHashCode();
+    }
+    
+}

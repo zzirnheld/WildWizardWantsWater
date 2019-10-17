ddf784cd1e04a2bd0b2e5473cb1bf8c7e6a0abb1
Zirnheld <zzirnhel@purdue.edu>
2019-10-17 14:39:11 -0400

    spell comparison works now

--- Assets/Scripts/PairComparer.cs
+++ Assets/Scripts/PairComparer.cs
@@ -7,12 +7,14 @@ public class PairComparer : IEqualityComparer<Pair>
 {
     public bool Equals(Pair x, Pair y)
     {
+        Debug.Log($"Comparing {x.ToString()} and {y.ToString()}, answer {(x.x == y.x && x.y == y.y) || (x.x == y.y && x.y == y.x)}");
         return (x.x == y.x && x.y == y.y) || (x.x == y.y && x.y == y.x);
     }
 
     public int GetHashCode(Pair obj)
     {
-        return obj.GetHashCode();
+        Debug.Log($"Returning hash code {obj.x * 10 + obj.y}");
+        return obj.x * 10 + obj.y;
     }
     
 }
